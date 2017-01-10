559114852df75dfdac5a2783671e6bf425819e85
Ethan Luts and Justin Stofiel <justin@>
2017-01-10 14:36:16 -0800

    add goodbye button and stop button on click function

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,11 @@ $(document).ready(function() {
     $("ul").prepend("<li>Hello!</li>");
   });
 
-  ("button#goodbye").click(function() {
+  $("button#goodbye").click(function() {
     $("ul").prepend("<li>Goodbye!</li>");
   });
 
-  ("button#stop").click(function() {
-    $("ul").prepend("<li>Stop copying me! :(</li>");
+  $("button#stop").click(function() {
+    $("ul").prepend("<li>Stop copying me!</li>");
   });
 });
